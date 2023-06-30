634897dee9296726f3fa2c843d0481db7ecfd25e
nevargass <leonardovargascardoso@hotmail.com>
2023-06-30 10:05:07 -0300

    First class created

--- ChessConsole/Program.cs
+++ ChessConsole/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using ChessConsole.BoardEntities;
+
+namespace ChessConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
